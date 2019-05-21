823b80326d2df9b268891a270f36b658c7267fcc
CCQ66666 <1959724905@qq.com>
2019-05-21 14:07:31 +0800

    core取消注册服务依赖

--- module/core/src/main/java/zut/cs/core/CoreApp.java
+++ module/core/src/main/java/zut/cs/core/CoreApp.java
@@ -3,28 +3,28 @@ package zut.cs.core;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
+//import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+//import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(scanBasePackages = {"zut.cs.*"})
 @EnableSwagger2Doc
-@EnableCaching
+//@EnableCaching
 //@EnableHystrixDashboard
 //@EnableHystrix
 //@EnableEurekaClient
 //@EnableFeignClients
 public class CoreApp {
-    @Bean
-    @LoadBalanced
-    public RestTemplate getBean(){
-        return new RestTemplate();
-    }
+//    @Bean
+//    @LoadBalanced
+//    public RestTemplate getBean(){
+//        return new RestTemplate();
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(CoreApp.class, args);
