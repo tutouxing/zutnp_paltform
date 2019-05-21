37189b82fb3b3dbb18fa771b83c79f26217c4a17
huangjingshuai <511308194@qq.com>
2019-05-21 14:30:45 +0800

    黄景帅周报

--- module/core/src/main/java/zut/cs/core/CoreApp.java
+++ module/core/src/main/java/zut/cs/core/CoreApp.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+//import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
