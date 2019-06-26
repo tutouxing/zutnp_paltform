31f2cb5f4b29e621ccf9545d006599ac5d9d9819
CCQ66666 <1959724905@qq.com>
2019-06-26 14:26:32 +0800

    picture

--- module/core/src/main/java/zut/cs/core/domain/Picture.java
+++ module/core/src/main/java/zut/cs/core/domain/Picture.java
@@ -1,14 +1,21 @@
 package zut.cs.core.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 import zut.cs.core.base.domain.BaseEntity;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Setter
 @Getter
 public class Picture extends BaseEntity {
     private String url;
+    @JsonIgnoreProperties(value = "pictures")
+    @ManyToOne
+    @JoinColumn(name = "contentId")
+    Content content;
 }
